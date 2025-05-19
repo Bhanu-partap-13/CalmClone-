688c9f6e5793f82e3900081d724caa3a33b2e59a
{Bhanu <partapbhanu516@gmail.com>
2025-05-19 11:29:58 +0530

    additional files

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
+    base: "/CalmClone-",
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
